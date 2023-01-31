e168df0439f4f6e5967d0d07a6588e32da11a3f7
ychebotaev <yury.79120345101@gmail.com>
2023-01-31 13:34:51 +0500

    fix uploaded image display - 2

--- src/pages/ArticleCreate/UploadImage/UploadImage.js
+++ src/pages/ArticleCreate/UploadImage/UploadImage.js
@@ -1,6 +1,6 @@
 import { useRef, useState } from 'react'
 import { useMutation } from '@tanstack/react-query'
-import { Skeleton } from '../../../components/common/Skeleton'
+import { Skeleton, KIND_UPDATING } from '../../../components/common/Skeleton'
 import { useApiClient, useLoadImage } from '../../../hooks'
 import imagePlaceholder from '../../../styles/images/image-placeholder.svg'
 import classes from './UploadImage.module.css'
@@ -10,8 +10,8 @@ export const UploadImage = () => {
   const fileRef = useRef()
   const apiClient = useApiClient()
   const [imgUrl, setImgUrl] = useState()
-  const { data: imageUrl } = useLoadImage(['post_image', imgUrl], imgUrl ? getBackendUrl() + imgUrl : undefined, imagePlaceholder)
-  const { mutate, isLoading } = useMutation(['posts', 'upload_image'], async () => {
+  const { data: imageUrl, isLoading } = useLoadImage(['post_image', imgUrl], imgUrl ? getBackendUrl() + imgUrl : undefined, imagePlaceholder)
+  const { mutate, isLoading: isUpdating } = useMutation(['posts', 'upload_image'], async () => {
     const formData = new FormData()
     formData.append('image', fileRef.current.files[0])
     const { data } = await apiClient.post('/posts/upload_image', formData)
@@ -25,22 +25,23 @@ export const UploadImage = () => {
 
   return (
     <>
-      {imgUrl ? (
-        <img
-          className={classes.uploadedImage}
-          src={getBackendUrl() + imgUrl}
-          alt=""
-          onClick={() => { fileRef.current.click() }}
-        />
-      ) : (
-        isLoading ? (
-          <Skeleton className={classes.imageSkeleton} />
-        ) : (
-          <div
-            className={classes.imagePlaceholder}
-            onClick={() => { fileRef.current.click() }}>
-            <img src={imageUrl} alt="" />
-          </div>
+      {isLoading ? <Skeleton className={classes.imageSkeleton} /> : (
+        isUpdating ? <Skeleton kind={KIND_UPDATING} className={classes.imageSkeleton} /> : (
+          imgUrl ? (
+            <img
+              src={imageUrl}
+              alt=""
+              className={classes.uploadedImage}
+              onClick={() => { fileRef.current.click() }}
+            />
+          ) : (
+            <div
+              className={classes.imagePlaceholder}
+              onClick={() => { fileRef.current.click() }}
+            >
+              <img src={imagePlaceholder} alt="" />
+            </div>
+          )
         )
       )}
       <input
