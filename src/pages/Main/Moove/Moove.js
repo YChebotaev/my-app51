01bd9244c09c141f75de526082adc0c12cec953f
ychebotaev <yury.79120345101@gmail.com>
2023-02-09 09:14:14 +0500

    Fix margins

--- src/pages/Main/Moove/Moove.js
+++ src/pages/Main/Moove/Moove.js
@@ -3,11 +3,11 @@ import classes from './Moove.module.css'
 
 export const Moove = () => (
   <Link to="/moove" className={classes.wrapper}>
+    <div className={classes.shark} />
     <div className={classes.supertitle}>MOOVE & MTS & SKOLKOVO</div>
     <div className={classes.moove}>
       <div className={classes.title}>О программе Moove</div>
       <div className={classes.subtitle}>Пройти в раздел и узнать больше</div>
     </div>
-    <div className={classes.shark} />
   </Link>
 )
